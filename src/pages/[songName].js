6f55cb5510927cc20deca9f59aa86d593775a87e
sinchandasgupta <sinchandsgpt9@gmail.com>
2023-03-22 02:38:44 +0530

    loading & error states

--- src/pages/[songName].js
+++ src/pages/[songName].js
@@ -2,18 +2,33 @@ import useSongList from "@/hooks/useSongList";
 import { useRouter } from "next/router";
 import { PuffLoader } from "react-spinners";
 import SearchSection from "@/components/SearchSection";
+import Head from "next/head";
 
 function SearchResults() {
   const router = useRouter();
   const { songName } = router.query;
   const { songList, error, isLoading } = useSongList(songName);
 
+  if (isLoading)
+    return (
+      <>
+        <SearchSection />
+        <section>
+          <PuffLoader />
+        </section>
+      </>
+    );
+
+  if (error) return <div>Something went wrong...</div>;
+
   return (
     <>
+      <Head>
+        <title>Results: {songName}</title>
+      </Head>
       <SearchSection />
       <section>
         <h2>Search Results:</h2>
-        {isLoading && <PuffLoader />}
         {songList?.map(({ result }) => (
           <div>{result.full_title}</div>
         ))}
