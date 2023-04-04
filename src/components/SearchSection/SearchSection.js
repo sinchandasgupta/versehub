4eb790287857e7c90104d2db8f89c305ca8c5589
sinchandasgupta <sinchandsgpt9@gmail.com>
2023-04-05 01:18:29 +0530

    Styles and some more animations

--- src/components/SearchSection/SearchSection.js
+++ src/components/SearchSection/SearchSection.js
@@ -1,12 +1,23 @@
 import SearchInput from "../SearchInput";
 import styles from "./SearchSection.module.css";
+import { motion } from "framer-motion";
 
 function SearchSection() {
   return (
-    <section className={styles.searchsection}>
+    <motion.section
+      initial={{ opacity: 0, y: -15, rotate: -20 }}
+      animate={{ opacity: 1, y: 0, rotate: 0 }}
+      transition={{
+        type: "spring",
+        stiffness: 300,
+        damping: 25,
+        duration: 0.25,
+      }}
+      className={styles.searchsection}
+    >
       <h2 className={styles.heading}>Search</h2>
       <SearchInput />
-    </section>
+    </motion.section>
   );
 }
 
