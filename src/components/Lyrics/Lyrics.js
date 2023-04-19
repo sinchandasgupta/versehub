e740cae8d2f4a1ca9f44170172b4cbd91a00b834
sinchandasgupta <sinchandsgpt9@gmail.com>
2023-04-19 20:19:26 +0530

    Lyrics: Switch to <img>

--- src/components/Lyrics/Lyrics.js
+++ src/components/Lyrics/Lyrics.js
@@ -1,16 +1,16 @@
-import Image from "next/image";
 import Link from "next/link";
 import styles from "./Lyrics.module.css";
 
 function Lyrics({ result }) {
   return (
     <Link href={`/lyrics/${result.id}`} className={styles.result}>
-      <Image
+      <img
         width={40}
         height={40}
         src={result.header_image_thumbnail_url}
         className={styles.thumbnail}
         alt={`${result.title} Album art`}
+        loading="lazy"
       />
       <div className={styles.stats}>
         <p className={styles.songName}>{result.title}</p>
